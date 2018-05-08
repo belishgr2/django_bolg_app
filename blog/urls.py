6ced7cb6b7e29170e61c7dd6104459a1cc671288
belishgr2 <belisugr2@gmail.com>
2018-05-08 17:15:52 +0900

    django app Second commit!

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 from django.shortcuts import render
 
 urlpatterns=[
-   url(r'^$',views.post_list,name='post_list'),
-
+    url(r'^$',views.post_list,name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$',views.post_detail,name='post_detail'),
 ]
