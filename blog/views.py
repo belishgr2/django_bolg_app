838a3f97acbfde16f50ca7da0658829c9ca860a5
belishgr2 <belisugr2@gmail.com>
2018-05-08 17:25:48 +0900

    django app Second commit!

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from django.utils import timezone
 def post_list(request):
     #  name = 'Django'
     # return HttpResponse('''<h1>Hello {Myname}</h1>'''.format(Myname=name))
-    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts=Post.objects.all()
     return render(request,'blog/post_list.html',{'posts': posts})
 
 def post_detail(request,pk):
